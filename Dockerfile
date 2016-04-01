132a180d9c977db89f5d8976fe12f083084ace84
Gottfried Ganßauge <ganssauge@gmx.de>
2016-04-01 13:32:03 +0200

    Initial import

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+FROM centos:6
+ENV JENKINS_UID 1000
+ENV JENKINS_HOME /var/lib/jenkins_home
+ENV SWARM_CLIENT_VERSION 2.0
+ENV SWARM_CLIENT swarm-client-${SWARM_CLIENT_VERSION}-jar-with-dependencies.jar
+RUN yum -y update && \
+    yum -y install \
+        gcc-c++ \
+        rpm-build \
+        ccache \
+        openssh-clients \
+        gettext \
+        java-1.8.0-openjdk-headless \
+	binutils-devel \
+        libcurl-devel \
+        expat-devel \
+        glibc-static \
+	ncurses-devel \
+        ncurses-static \
+        readline-devel \
+        openssl-devel \
+        zlib-devel \
+        zlib-static \
+        tcl-devel \
+        python-devel \
+        git \
+        wget \
+        libxslt \
+        docbook-utils \
+        docbook-dtds \
+        docbook-xsl \
+        bzip2-devel \
+        tar \
+        unzip
+
+COPY startup.sh /usr/bin/startup.sh
+ENTRYPOINT ["/usr/bin/startup.sh"]
+
+COPY setup-slave.sh /tmp/setup-slave.sh
+ADD https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz /tmp/Python-2.7.10.tgz
+ADD http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/$SWARM_CLIENT_VERSION/$SWARM_CLIENT $JENKINS_HOME/$SWARM_CLIENT
+
+ENV OSRBOOSTHOME /opt/boost
+ENV ICU_PATH /opt/icu
+# now that all dependencies are complete setup the slave
+RUN /bin/sh /tmp/setup-slave.sh && rm -f /tmp/setup-slave.sh
+USER jenkins_slave
---
+++ setup-slave.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# Build locale used for message catalogs
+locale-gen de_DE@euro || localedef --quiet -c -i de_DE -f ISO-8859-1 de_DE@euro
+
+ccache=ccache-3.2.3.tar.xz
+icu=icu4c-51_2-src.tgz 
+
+# set timezone
+cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+
+# add our user
+groupadd -g $JENKINS_UID jenkins_slave
+useradd -d $JENKINS_HOME -s /bin/bash -m jenkins_slave -u $JENKINS_UID -g jenkins_slave
+echo jenkins_slave:jpass | chpasswd
+
+# make sure permissions are correct
+chmod +x /usr/bin/startup.sh
+chown -R $JENKINS_UID:$JENKINS_UID $JENKINS_HOME
+
+mkdir -p /tmp/build
+icuprefix=${ICU_PATH-/opt/icu51}
+boostprefix=${OSRBOOSTHOME-/opt/boost-1.58}
+
+cd /tmp/build
+wget -q http://samba.org/ftp/ccache/$ccache
+tar xf $ccache
+cd `basename $ccache .tar.xz`
+./configure
+make
+make install
+
+cd /tmp/build
+wget -q http://download.icu-project.org/files/icu4c/51.2/$icu
+tar xf $icu
+cd icu/source
+./configure --prefix=$icuprefix && make && make install
+echo $icuprefix/lib > /etc/ld.so.conf.d/icu.conf
+ldconfig
+
+cd /tmp/build
+wget -q http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.gz/download
+tar xf download 
+cd boost_1_58_0/
+./bootstrap.sh --prefix=$boostprefix --with-icu=$icuprefix
+./b2 install
+echo $boostprefix/lib > /etc/ld.so.conf.d/boost.conf
+ldconfig
+rm -rf /tmp/build
+
+# compile python-2.7
+cd /tmp
+tar xf Python-2.7.10.tgz
+cd Python-2.7.10
+./configure
+make all install
+# add pip and virtualenv to the newly build python
+wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py
+/usr/local/bin/python get-pip.py
+/usr/local/bin/pip install virtualenv
+
+# add a virtualenv for build use
+su jenkins_slave -c "virtualenv -p /usr/local/bin/python ~jenkins_slave/py"
+su jenkins_slave -c "~jenkins_slave/py/bin/pip install sshed"
